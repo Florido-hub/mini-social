ce3041304b079892686b0f1d919ad27f00c9e92b
Florido-hub <floridodinizz@gmail.com>
2026-03-19 18:37:17 -0300

    fix: remove imports desnecessarios

--- src/main/java/com/florido/workshopmongo/resources/UserResource.java
+++ src/main/java/com/florido/workshopmongo/resources/UserResource.java
@@ -1,14 +1,11 @@
 package com.florido.workshopmongo.resources;
 
 import com.florido.workshopmongo.domain.User;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
