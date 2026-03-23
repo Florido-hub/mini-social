9fd97f51cf01aa025780e70564e1a7d30da06d13
Florido-hub <floridodinizz@gmail.com>
2026-03-23 10:01:36 -0300

    feat(post): cria entidade Post

--- src/main/java/com/florido/workshopmongo/common/model/document/Post.java
+++ src/main/java/com/florido/workshopmongo/common/model/document/Post.java
@@ -17,4 +17,27 @@ public class Post implements Serializable {
 
     @Id
     private String id;
+    private Date date;
+    private String title;
+    private String body;
+    private User author;
+
+    public Post(Date date, String title, String body, User author) {
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
