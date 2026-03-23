adda3849287230b55ca49671f5444bda774e3127
Florido-hub <floridodinizz@gmail.com>
2026-03-23 09:56:51 -0300

    refactor: refatora mappers

--- src/main/java/com/florido/workshopmongo/common/mapper/PostMapper.java
+++ src/main/java/com/florido/workshopmongo/common/mapper/PostMapper.java
@@ -1,4 +1,13 @@
 package com.florido.workshopmongo.common.mapper;
 
+import com.florido.workshopmongo.common.model.document.User;
+import com.florido.workshopmongo.query.user.UserDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface PostMapper {
+
+    UserDTO toDto(User user);
+
+    User toModel(UserDTO usetDTO);
 }
--- src/main/java/com/florido/workshopmongo/common/model/document/User.java
+++ src/main/java/com/florido/workshopmongo/common/model/document/User.java
@@ -2,6 +2,7 @@ package com.florido.workshopmongo.common.model.document;
 
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -9,6 +10,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 @Data
+@NoArgsConstructor
 @Document(collection = "user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -18,11 +20,7 @@ public class User implements Serializable {
     private String name;
     private String email;
 
-    public User() {
-    }
-
-    public User(String id, String name, String email) {
-        this.id = id;
+    public User(String name, String email) {
         this.name = name;
         this.email = email;
     }
