32907115dc0cf3b6b78ccac543a09ff4c64ed5c3
Florido-hub <floridodinizz@gmail.com>
2026-03-25 15:04:26 -0300

    feat(usuario): refatorando contrato de deleção de usuario

--- src/main/java/com/florido/workshopmongo/common/repository/PostRepository.java
+++ src/main/java/com/florido/workshopmongo/common/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package com.florido.workshopmongo.common.repository;
 
 import com.florido.workshopmongo.common.model.document.Post;
+import com.florido.workshopmongo.query.post.AuthorDTO;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+    List<Post> findByAuthor(AuthorDTO author);
 }
